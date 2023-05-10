4ec0d2d2e1669b4914f98a45d75617c87da6cdef
xonika9 <xonika9@gmail.com>
2023-05-10 12:00:40 +0300

    form works

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,48 @@
 import './App.css';
 import { Form, Select, DatePicker, TimePicker, Input, Button } from 'antd';
 
+const { Option } = Select;
 const { TextArea } = Input;
 
 const App = () => {
   const [form] = Form.useForm();
 
+  const handleClear = () => {
+    form.resetFields();
+  };
+
+  const handleSend = () => {
+    console.log(JSON.stringify(form.getFieldsValue()));
+  };
+
+  const floors = Array.from({ length: 25 }, (_, i) => i + 3);
+  const rooms = Array.from({ length: 10 }, (_, i) => i + 1);
+
   return (
     <Form form={form}>
       <Form.Item name="tower">
-        <Select placeholder="Select tower"></Select>
+        <Select placeholder="Select tower">
+          <Option value="A">A</Option>
+          <Option value="B">B</Option>
+        </Select>
       </Form.Item>
       <Form.Item name="floor">
-        <Select placeholder="Select floor"></Select>
+        <Select placeholder="Select floor">
+          {floors.map((floor) => (
+            <Option key={floor} value={floor}>
+              {floor}
+            </Option>
+          ))}
+        </Select>
       </Form.Item>
       <Form.Item name="room">
-        <Select placeholder="Select room"></Select>
+        <Select placeholder="Select room">
+          {rooms.map((room) => (
+            <Option key={room} value={room}>
+              {room}
+            </Option>
+          ))}
+        </Select>
       </Form.Item>
       <Form.Item name="date">
         <DatePicker />
@@ -27,8 +54,10 @@ const App = () => {
         <TextArea placeholder="Comments" />
       </Form.Item>
       <Form.Item>
-        <Button>Send</Button>
-        <Button>Clear</Button>
+        <Button type="primary" onClick={handleSend}>
+          Send
+        </Button>
+        <Button onClick={handleClear}>Clear</Button>
       </Form.Item>
     </Form>
   );
